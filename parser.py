5e146a095b35f4a27ddbd3a3ab8a6e24abb24d19
rishikanthc <rishikanthc@gmail.com>
2016-05-01 10:12:09 -0400

    fixed bug - error with dont care

--- parser.py
+++ parser.py
@@ -18,26 +18,27 @@ def build_code_format_dict():
 		code_format_dict[ command[0] ] = command[1]
 	code_format.close()
 
-
 def parsing():
 	coding = open("code.txt",'r')
-
-	a=7
 	output_file = open("output.txt",'w')
 	for line in coding:
 		command=[]
 		text = line.strip()
 		code = text.split(" ")
 		command.extend( opcode_dict[code[0]] )
-		args = code[1].split(",")
-		formatter = code_format_dict[code[0]].split(",")
-		print "format", formatter, "length", len(formatter)
-		print "arguements",args, "length", len(args)
-		if len(args) == 0:
+
+
+		if len(code) == 1:
+			print "no args"
 			diff = 32-len(command)
 			for z in xrange(0,diff):
-				command.extend(binary(num=0,length=7))
+				command.extend(str('0'))
+			#print command
 		else:
+			args = code[1].split(",")
+			formatter = code_format_dict[code[0]].split(",")
+			print "format", formatter, "length", len(formatter)
+			print "arguements",args, "length", len(args)
 			if len(args) < len(formatter):
 				diff = len(formatter) - len(args)
 				for z in xrange(0,diff):
@@ -45,21 +46,12 @@ def parsing():
 			for x in range(len(args)-1,-1,-1):
 				#print args[x]
 				command.extend(binary(num=int(args[x]),length=int(formatter[x]) ))
-				#arg_val = (args[x].split(":"))[1]  #code input is ra:4 rt:9 i:4
-				#print arg_val
-				#command.extend(binary(num=int(arg_val),length=int(formatter[x]) ))
-			print "command",command
+		print "command",command
 			#print len(command)
-			for y in xrange(0,len(command)):
-				output_file.write(command[y])
-				if ( (y+1) % 8 == 0) and (y>0):
-					output_file.write("\n")
-			#command.extend(args[0])
-			#command.extend(bin(int(code[1])))
-
-			#command.extend(format(int(int(args[0]) & 0xff ), '07b'))
-			#command.extend(binary(num=int(args[0]),length=a))
-			#command.extend("{0:b}".format(int(args[0]) & 0xff ))
+		for y in xrange(0,len(command)):
+			output_file.write(command[y])
+			if ( (y+1) % 8 == 0) and (y>0):
+				output_file.write("\n")
 
 
 def binary(num, pre='', length=8, spacer=0):
@@ -67,6 +59,4 @@ def binary(num, pre='', length=8, spacer=0):
 
 build_dict()
 build_code_format_dict()
-#print opcode_dict
-#print code_format_dict
 parsing()
